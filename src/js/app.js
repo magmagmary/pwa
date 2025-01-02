e3c5fc9906379f88e0f57d41f75b3e99f88f2d9d
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-02 15:16:04 +0100

    feat: utilize relative path for service worker

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 let deferredPrompt;
 
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('sw.js')
     .then((reg) => {
         console.log('Service Worker Registered');
     })
