8abb000a15837384fa0741bb58b9c49c18ed95bb
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-03 12:10:11 +0100

    feat: cache assets statically

--- sw.js
+++ sw.js
@@ -1,5 +1,24 @@
+const cachedAssets = [
+    '/',
+    '/index.html',
+    '/src/js/app.js',
+    '/src/js/feed.js',
+    '/src/js/material.min.js',
+    '/src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+];
+
 self.addEventListener('install', function(event) {
     console.log('Service Worker: installing.' , event);
+    event.waitUntil(
+        caches.open('static').then((cache)=>{
+            console.log('service worker: pre-caching app shell');
+
+            cache.addAll(cachedAssets);
+        })
+    );
 });
 
 self.addEventListener('activate', function(event) {
@@ -9,6 +28,12 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        fetch(event.request)
+        caches.match(event.request).then((response)=>{
+            if(response){
+                return response;
+            } else {
+                return fetch(event.request);
+            }
+        })
     );
 });
