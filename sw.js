bf2c785b70e029d52ea7dd025a7e3741153eb82c
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-03 10:48:16 +0100

    return claim

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
     console.log('Service Worker: activating.' , event);
+    return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
