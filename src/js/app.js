bd2c8298ddd7365cb0514f43252b1767250570dd
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-09 18:14:11 +0100

    enable notification

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 let deferredPrompt;
 
+const enableNotificationsButtons = document.querySelectorAll(
+  ".enable-notifications"
+);
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("sw.js")
@@ -16,3 +20,32 @@ window.addEventListener("beforeinstallprompt", (e) => {
   deferredPrompt = e;
   return false;
 });
+
+const displayConfirmNotification = (title) => {
+  const options = {
+    body: "some Random body!",
+  };
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.ready.then((sw) => {
+      sw.showNotification("hihi", options);
+    });
+  }
+};
+
+const askForNotificationPermission = () => {
+  Notification.requestPermission((result) => {
+    if (result !== "granted") {
+      return console.log("No notification permission granted");
+    }
+    displayConfirmNotification(
+      "You successfully subscribed to our notification service!"
+    );
+  });
+};
+
+if ("Notification" in window) {
+  for (button of enableNotificationsButtons) {
+    button.style.display = "inline-block";
+    button.addEventListener("click", askForNotificationPermission);
+  }
+}
