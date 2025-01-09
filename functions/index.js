7bec3c6a90ed42e579ac1dee215fb2b8b8263974
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-09 17:13:29 +0100

    feat: firebase  functions

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const admin = require("firebase-admin");
+const cors = require("cors")({ origin: true });
+
+const serviceAccount = require("./mgm-pwa-firebase.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mgm-pwa-default-rtdb.europe-west1.firebasedatabase.app",
+});
+
+exports.storePost = onRequest((request, response) => {
+  cors(request, response, () => {
+    admin
+      .database()
+      .ref("posts")
+      .push(request.body)
+      .then(() => {
+        response.status(201).json({
+          message: "post created successfully",
+          id: request.body.id,
+        });
+      })
+      .catch((err) =>
+        response.status(500).json({
+          err,
+        })
+      );
+  });
+});
