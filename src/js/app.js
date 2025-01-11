65001131fb4912d822a8a471f2c055a095dbadf2
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-11 19:14:27 +0100

    feat: subscribing a push event

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+const VAPID_PUBLIC_KEY =
+  "BHjSoVgchkHG7ccuFGFi1lDWqPJUeldmin6VE6FuwGhm2TL5jTlMpMvyAgv6eoKuqOMwwEYrDIwivTCdghQsUaQ";
 let deferredPrompt;
 
 const enableNotificationsButtons = document.querySelectorAll(
@@ -21,17 +23,17 @@ window.addEventListener("beforeinstallprompt", (e) => {
   return false;
 });
 
-const displayConfirmNotification = (title) => {
+const displayConfirmNotification = (title, body = "") => {
   const options = {
-    body: "some Random body!",
+    body,
     icon: "src/images/icons/app-icon-96x96.png",
     image: "src/images/sf-boat.jpg", // not supported in all devices
     dir: "ltr",
     lang: "en-US", // BCP 47
     vibrate: [100, 50, 200], // vibration, pause, vibration
     badge: "src/images/icons/app-icon-96x96.png",
-    tag: "confirm-notification", // to avoid multiple notifications
-    renotify: false, // to make the device vibrate again
+    // tag: "confirm-notification", // to avoid multiple notifications
+    // renotify: false, // to make the device vibrate again
     actions: [
       {
         action: "confirm",
@@ -45,11 +47,62 @@ const displayConfirmNotification = (title) => {
       },
     ],
   };
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.ready.then((sw) => {
-      sw.showNotification(title, options);
-    });
+  navigator.serviceWorker.ready.then((sw) => {
+    sw.showNotification(title, options);
+  });
+};
+
+const urlBase64ToUint8Array = (base64String) => {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
   }
+
+  return outputArray;
+};
+
+const configurePushSubscription = () => {
+  navigator.serviceWorker.ready.then((sw) => {
+    sw.pushManager
+      .getSubscription()
+      .then((sub) => {
+        if (sub === null) {
+          // create a new subscription
+          return sw.pushManager.subscribe({
+            userVisibleOnly: true, // only show notification if the user is on the page
+            applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+          });
+        }
+      })
+      .then((newSub) => {
+        return fetch(
+          "https://mgm-pwa-default-rtdb.europe-west1.firebasedatabase.app/subscriptions.json",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify(newSub),
+          }
+        );
+      })
+      .then((res) => {
+        if (res.ok) {
+          displayConfirmNotification("You successfully subscribe!");
+        }
+      })
+      .catch((err) => {
+        console.error("Error while subscribing", err);
+      });
+  });
 };
 
 const askForNotificationPermission = () => {
@@ -57,11 +110,17 @@ const askForNotificationPermission = () => {
     if (result !== "granted") {
       return console.log("No notification permission granted");
     }
-    displayConfirmNotification("You successfully subscribed");
+
+    configurePushSubscription();
+
+    // displayConfirmNotification(
+    //   "You successfully subscribed",
+    //   "Thanks for subscribing"
+    // );
   });
 };
 
-if ("Notification" in window) {
+if ("Notification" in window && "serviceWorker" in navigator) {
   for (button of enableNotificationsButtons) {
     button.style.display = "inline-block";
     button.addEventListener("click", askForNotificationPermission);
