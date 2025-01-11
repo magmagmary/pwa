dd8654968deff16a91ae9c34fe80fecce7636d82
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-11 19:42:11 +0100

    feat: send push messages

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,10 @@ const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const admin = require("firebase-admin");
 const cors = require("cors")({ origin: true });
+const webPush = require("web-push");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // the config file is not included in the repo. You should create your own
 const serviceAccount = require("./mgm-pwa-firebase.json");
@@ -18,6 +22,33 @@ exports.storePost = onRequest((request, response) => {
       .ref("posts")
       .push(request.body)
       .then(() => {
+        webPush.setVapidDetails(
+          "mailto:magmagmary70@gmail.com",
+          process.env.VAPID_PUBLIC_KEY,
+          process.env.VAPID_PRIVATE_KEY
+        );
+
+        return admin.database().ref("subscriptions").once("value");
+      })
+      .then((subscriptions) => {
+        // biome-ignore lint/complexity/noForEach: <explanation>
+        subscriptions.forEach((sub) => {
+          const { endpoint, keys } = sub.val();
+          const pushConfig = {
+            endpoint,
+            keys,
+          };
+
+          webPush
+            .sendNotification(
+              pushConfig,
+              JSON.stringify({
+                title: "New Post",
+                content: "New post added!",
+              })
+            )
+            .catch((err) => logger.error(err));
+        });
         response.status(201).json({
           message: "post created successfully",
           id: request.body.id,
