3f46c98cf61a8577300da5197d38a5b14ed0c3e6
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-25 14:08:38 +0100

    add: push notification and background sync

--- service-worker.js
+++ service-worker.js
@@ -217,3 +217,90 @@ workboxSW.precache([
     "revision": "a9890beda9e5f17e4c68f42324217941"
   }
 ]);
+
+self.addEventListener("sync", (event) => {
+  console.log("Service Worker: Background syncing", event);
+
+  if (event.tag === "sync-new-posts") {
+    console.log(">>>>Syncing new posts");
+
+    event.waitUntil(
+      readAllData(POST_SYNC_STORE).then((data) => {
+        for (const post of data) {
+          const formData = new FormData();
+
+          for (const key in post) {
+            formData.append(key, post[key]);
+          }
+
+          fetch(generateUrl("storePost"), {
+            method: "POST",
+            body: formData,
+          })
+            .then((res) => res.json())
+            .then(({ message, id }) => {
+              console.log(message);
+              deleteItemFromData(POST_SYNC_STORE, id);
+            })
+            .catch((err) => {
+              console.error("Error while sending data", err);
+            });
+        }
+      })
+    );
+  }
+});
+
+self.addEventListener("notificationclick", (event) => {
+  const notification = event.notification;
+  const action = event.action;
+
+  if (action === "confirm") {
+    console.log("confirm was chosen");
+
+    return notification.close();
+  }
+
+  event.waitUntil(
+    clients.matchAll().then((clients) => {
+      const client = clients.find((cli) => cli.visibilityState === "visible");
+      notification.close();
+      if (client) {
+        client.navigate(notification.data.url);
+        return client.focus();
+      }
+
+      clients.openWindow(notification.data.url);
+    })
+  );
+});
+
+self.addEventListener("notificationclose", (event) => {
+  console.log("notification was closed", event);
+});
+
+self.addEventListener("push", (event) => {
+  console.log("Push notification received", event);
+  let data = {
+    title: "New!",
+    content: "Something new happened!",
+    openUrl: "/",
+  };
+
+  if (event.data) {
+    data = JSON.parse(event.data.text());
+  }
+
+  const notificationOptions = {
+    body: data.content,
+    icon: "/src/images/icons/app-icon-96x96.png",
+    badge: "/src/images/icons/app-icon-96x96.png",
+    data: {
+      url: data.openUrl,
+    },
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, notificationOptions)
+  );
+});
