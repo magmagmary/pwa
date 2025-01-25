10cc6e604247240bd1947f69ca35728a614f462b
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-25 13:12:43 +0100

    add: some further config to cache

--- sw-base.js
+++ sw-base.js
@@ -7,6 +7,10 @@ workboxSW.router.registerRoute(
   /.*(googleapis|gstatic)\.com/,
   workboxSW.strategies.staleWhileRevalidate({
     cacheName: "google-fonts",
+    cacheExpiration: {
+      maxEntries: 3,
+      maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+    },
   }) // cache then network
 );
 
